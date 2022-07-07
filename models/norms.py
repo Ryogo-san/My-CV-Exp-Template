9929b9bb1e91e19ce3ab3318c7146609595f7b2f
Ryogo-san <ritosetsugekka0226@gmail.com>
2022-07-07 15:35:22 +0900

    [add] attention

---
+++ models/norms.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        var = torch.var(x, dim=1, unbiased=False, keepdim=True)
+        mean = torch.mean(x, dim=1, keepdim=True)
+
+        return (x - mean) / (var + self.eps).sqrt() * self.g + self.b
